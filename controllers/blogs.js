fad861961820b6109ed5993914c53f6cddc4f00a
OptimisticTrousers <theoptimistictrouser@protonmail.com>
2025-07-28 18:25:47 -0400

    Complete Exercise 13.12.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -26,7 +26,13 @@ const blogFinder = async (req, res, next) => {
 };
 
 router.get("/", async (req, res) => {
-  const blogs = await Blog.findAll();
+  const blogs = await Blog.findAll({
+    attributes: { exclude: ["userId"] },
+    include: {
+      model: User,
+      attributes: ["name"],
+    },
+  });
   console.log(JSON.stringify(blogs, null, 2));
   res.json(blogs);
 });
